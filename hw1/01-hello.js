6c8eab2c8dc83de3b36aff134601280ed30c8249
Wenkang <166893688+Wenkangy@users.noreply.github.com.>
2025-04-20 13:48:13 -0700

    hw1

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is wenkang a computer science major. Love art and video Games."
+  );
 }
